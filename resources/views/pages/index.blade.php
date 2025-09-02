12cc688294994d5d21985690919116017b746214
marco76tv <marco.sottana@gmail.com>
2025-09-02 09:36:28 +0200

    .

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,18 @@
+<<<<<<< HEAD
 <x-layouts.app>
+=======
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+>>>>>>> 49645c4 (.)
     <div class="content">
         <h2>Welcome to Zero Theme</h2>
         <p>This is the default page for the Zero theme. You can customize this page as needed.</p>
     </div>
+<<<<<<< HEAD
 </x-layouts.app>
+=======
+@endsection
+>>>>>>> 49645c4 (.)
